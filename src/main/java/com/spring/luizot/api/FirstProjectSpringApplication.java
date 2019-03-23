7f78c3612529afe4e266363ed236c9576157e2fa
Luiz Otávio Martins Fonseca <luiz.fonseca@MJV.net>
2019-03-22 22:25:11 -0300

    Versão inicial do Projeto Spring

---
+++ src/main/java/com/spring/luizot/api/FirstProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.luizot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringApplication.class, args);
+	}
+
+}
